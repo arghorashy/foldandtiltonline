bab2d24d724511a99337fa13e85894ba2208d0a9
Afsheen Ghorashy <arghorashy@gmail.com>
2014-10-19 17:10:57 -0400

    Added styles...

---
+++ app/controllers/template_controller.rb
@@ -0,0 +1,6 @@
+class TemplateController < ApplicationController
+	def index
+		render 'fold_and_tilt/template_index'
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   post 'fold_and_tilt/uploadPicture'
 
+  get 'template/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
